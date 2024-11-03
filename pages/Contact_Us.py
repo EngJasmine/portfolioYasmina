713c2cf3ef081707d67b9253bb939ed760b56e88
Yasmine-Nasri <yasmine_nasri@yahoo.fr>
2024-11-03 15:34:49 -0500

    Send email using the form

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,6 +1,7 @@
 from cProfile import label
 
 import streamlit as st
+from send_email import send_email
 
 st.header('Contact Me')
 
@@ -8,6 +9,12 @@ with st.form(key='email_form'):
     #st.write('Your email address ')
     email_input=st.text_input(label='Your email address',key='email')
     message_input=st.text_area(label='Your message',key='message')
+    message = f'''Subject: New Email  
+    
+    From : {email_input}
+
+    {message_input}'''
     submit_button=st.form_submit_button()
     if submit_button:
-        st.write()
+        send_email(message)
+        st.info("Your email was sent successfully")
--- send_email.py
+++ send_email.py
@@ -1,27 +1,25 @@
 import smtplib
 import ssl
 
-# SMTP server configuration
-host = "smtp.gmail.com"
-port = 465
-username = "reema5khamassi@gmail.com"
-password = "ildt ermu tozq spci"  # Use an App Password if you have 2FA enabled
 
-# Recipient email
-receiver = 'reema5khamassi@gmail.com'
-message = '''Subject: Test Email
+def send_email(message):
+    # SMTP server configuration
+    host = "smtp.gmail.com"
+    port = 465
+    username = "reema5khamassi@gmail.com"
+    password = "ildt ermu tozq spci"  # Use an App Password if you have 2FA enabled
+    # Recipient email
+    receiver = 'reema5khamassi@gmail.com'
 
-Hi, how are you?
-Bye!'''
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+    try:
+        # Connect to the server and send email
+        with smtplib.SMTP_SSL(host=host, port=port, context=context) as server:
+            server.login(username, password)
+            server.sendmail(username, receiver, message)
+            print("Email sent successfully!")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-# Create a secure SSL context
-context = ssl.create_default_context()
 
-try:
-    # Connect to the server and send email
-    with smtplib.SMTP_SSL(host=host, port=port, context=context) as server:
-        server.login(username, password)
-        server.sendmail(username, receiver, message)
-        print("Email sent successfully!")
-except Exception as e:
-    print(f"An error occurred: {e}")
